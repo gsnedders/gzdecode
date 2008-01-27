557b34ccdb9a11b521e04a98ea130e88f6653d29
Geoffrey Sneddon <geoffers@gmail.com>
2008-01-27 16:21:57 +0000

    Start to add flag specific fields

--- gzdecode.php
+++ gzdecode.php
@@ -107,13 +107,13 @@ class gzdecode
 		if ($this->compressed_size >= $this->min_compressed_size)
 		{
 			// Check ID1, ID2, and CM
-			if (substr($data, 0, 3) !== "\x1F\x8B\x08")
+			if (substr($this->compressed_data, 0, 3) !== "\x1F\x8B\x08")
 			{
 				return false;
 			}
 			
 			// Get the FLG (FLaGs)
-			$this->flags = ord($data[3]);
+			$this->flags = ord($this->compressed_data[3]);
 		
 			// FLG bits above (1 << 4) are reserved
 			if ($flg > 0x1F)
@@ -131,10 +131,16 @@ class gzdecode
 			}
 			
 			// Get the XFL (eXtra FLags)
-			$this->XFL = ord($data[$this->position++]);
+			$this->XFL = ord($this->compressed_data[$this->position++]);
 		
 			// Get the OS (Operating System)
-			$this->OS = ord($data[$this->position++]);
+			$this->OS = ord($this->compressed_data[$this->position++]);
+			
+			// Parse the FEXTRA
+			if (!$this->fextra())
+			{
+				return false;
+			}
 		}
 		else
 		{
@@ -148,11 +154,69 @@ class gzdecode
 		static $big_endian = (current(unpack('S', "\x00\x01")) === 1) ? true : false;
 		
 		// MTIME
-		$mtime = substr($data, $this->position, 4);
+		$mtime = substr($this->compressed_data, $this->position, 4);
 		if ($big_endian)
 		{
 			$mtime = strrev($mtime);
 		}
 		$this->MITME = current(unpack('l', $mtime));
 		$this->position += 4;
+		
+		// Nothing can fail
+		return true;
+	}
+	
+	private function fextra()
+	{
+		if ($this->flags & 4)
+		{
+			// Read subfield IDs
+			$this->SI1 = $this->compressed_data[$this->position++];
+			$this->SI2 = $this->compressed_data[$this->position++];
+			
+			// Get the length of the extra field
+			$len = current(unpack('v', substr($this->compressed_data, $this->position, 2)));
+			$position += 2;
+			
+			// Check the length of the string is still valid
+			$this->min_compressed_size += $len + 4;
+			if ($this->compressed_size >= $this->min_compressed_size)
+			{
+				// Set the extra field to the given data
+				$this->extra_field = substr($this->compressed_data, $this->position, $len);
+				$this->position += $len;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		// Either there is no extra field or it is valid
+		return true;
+	}
+	
+	private function fname()
+	{
+		if ($this->flags & 8)
+		{
+			// Get the length of the extra field
+			$len = strspn($this->compressed_data, "\x00", $this->position)));
+			
+			// Check the length of the string is still valid
+			$this->min_compressed_size += $len + 1;
+			if ($this->compressed_size >= $this->min_compressed_size)
+			{
+				// Set the original filename to the given string
+				$this->filename = substr($this->compressed_data, $this->position, $len);
+				$this->position += $len + 1;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		// Either there is no filename or it is valid
+		return true;
 	}
